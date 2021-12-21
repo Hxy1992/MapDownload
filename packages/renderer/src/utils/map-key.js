1d4fcebe92cc705488075442da5d286e1f0d8c97
huangxy <496917621@qq.com>
2021-12-21 09:32:21 +0800

    修改

--- packages/renderer/src/utils/map-key.js
+++ packages/renderer/src/utils/map-key.js
@@ -3,23 +3,6 @@
 const TDTKEY = 'map-key-tdt';
 const MAPBOXKEY = 'map-key-mapbox';
 
-// const MapKey = {
-//   _tdtKey: '',
-//   get tdtKey() {
-//     return this._tdtKey;
-//   },
-//   set tdtKey(val) {
-//     this._tdtKey = val;
-//   },
-//   _mapboxKey: '',
-//   get mapboxKey() {
-//     return this._mapboxKey;
-//   },
-//   set mapboxKey(val) {
-//     this._mapboxKey = val;
-//   },
-// };
-
 /**
  * 获取地图key
  * @returns 地图key
@@ -29,7 +12,6 @@ export function getKeys() {
     tdtKey: localStorage.getItem(TDTKEY),
     mapboxKey: localStorage.getItem(MAPBOXKEY),
   };
-  // return MapKey;
 }
 /**
  * 设置key
@@ -38,6 +20,4 @@ export function getKeys() {
 export function setKeys(param) {
   localStorage.setItem(TDTKEY, param?.tdtKey);
   localStorage.setItem(MAPBOXKEY, param?.mapboxKey);
-  // MapKey.tdtKey = param?.tdtKey;
-  // MapKey.mapboxKey = param?.mapboxKey;
 }
